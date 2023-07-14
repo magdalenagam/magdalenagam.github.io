f4a2aa3040a313c89c81e03d852d8b7ea8f6bb0f
Magdalena Gambuli <91838620+magdalenagam@users.noreply.github.com>
2023-07-13 21:23:28 -0300

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -112,7 +112,7 @@ const config = {
     snippetVersion: 6,
   },*/
   themeConfig: {
-    defaultTheme: 'customTheme',
+    defaultTheme: 'procyon',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
